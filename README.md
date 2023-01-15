b0a2c9090e77770f4241916e8095764db94d4519
LeilaKarzand <l.karzand@yahoo.com>
2023-01-15 15:53:42 +0330

    fix some bugs and add requirements

---
+++ README.md
@@ -0,0 +1,11 @@
+# FiveApp messenger
+
+test user:
+
+    email: l.karzand@yahoo.com
+    password: 123
+
+to run:
+
+    pip install -r requirements.txt
+    python manage.py runserver
--- apps/member/views.py
+++ apps/member/views.py
@@ -6,7 +6,7 @@ from django.views.generic import UpdateView
 from django.views.generic.base import View
 from rest_framework.views import APIView
 from django.contrib.sites.shortcuts import get_current_site
-from django.utils.encoding import force_bytes, force_text
+from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.template.loader import render_to_string
 from django.core.mail import EmailMessage
@@ -65,7 +65,7 @@ def email_confirmation(request, uidb64, token):
     activated.
     """
     try:
-        uid = force_text(urlsafe_base64_decode(uidb64))
+        uid = force_str(urlsafe_base64_decode(uidb64))
         user = Member.objects.get(pk=uid)
     except(TypeError, ValueError, OverflowError, Member.DoesNotExist):
         user = None
